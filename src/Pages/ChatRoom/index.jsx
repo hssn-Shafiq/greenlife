f65b730788acbecf0e793a9df216ee937cafca14
Auknji <124204197+Auknji@users.noreply.github.com>
2024-08-27 12:46:17 +0500

    chatroom integrated

--- src/Pages/ChatRoom/index.jsx
+++ src/Pages/ChatRoom/index.jsx
@@ -21,8 +21,9 @@ const ChatRoom = () => {
               </span>
             </div>
             <div className="chat__conversation-board__message__context">
-              <div className="chat__conversation-board__message__bubble">
+              <div className="chat__conversation-board__message__bubble d-flex flex-column">
                 {" "}
+                <span className="user_name">Monika Figi (Ward Name)</span>
                 <span>
                   Somewhere stored deep, deep in my memory banks is the phrase
                   "It really whips the llama's ass".
@@ -84,8 +85,9 @@ const ChatRoom = () => {
               </span>
             </div>
             <div className="chat__conversation-board__message__context">
-              <div className="chat__conversation-board__message__bubble">
+              <div className="chat__conversation-board__message__bubble d-flex flex-column">
                 {" "}
+                <span className="user_name">Monika Figi (Ward Name)</span>
                 <span>Think the guy that did the voice has a Twitter?</span>
               </div>
             </div>
@@ -144,8 +146,10 @@ const ChatRoom = () => {
               </span>
             </div>
             <div className="chat__conversation-board__message__context">
-              <div className="chat__conversation-board__message__bubble">
+              <div className="chat__conversation-board__message__bubble d-flex flex-column">
                 {" "}
+                <span className="user_name">Monika Figi (Ward Name)</span>
+
                 <span>WE MUST FIND HIM!!</span>
               </div>
               <div className="chat__conversation-board__message__bubble">
@@ -208,9 +212,76 @@ const ChatRoom = () => {
               </span>
             </div>
             <div className="chat__conversation-board__message__context">
+              <div className="chat__conversation-board__message__bubble d-flex flex-column">
+                {" "}
+                <span className="user_name m-0 text-left">Admin</span>
+                <span>Winamp's still an essential.</span>
+              </div>
+            </div>
+            <div className="chat__conversation-board__message__options">
+              <button className="btn-icon chat__conversation-board__message__option-button option-item emoji-button">
+                <svg
+                  className="feather feather-smile sc-dnqmqq jxshSx"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  aria-hidden="true"
+                >
+                  <circle cx={12} cy={12} r={10} />
+                  <path d="M8 14s1.5 2 4 2 4-2 4-2" />
+                  <line x1={9} y1={9} x2="9.01" y2={9} />
+                  <line x1={15} y1={9} x2="15.01" y2={9} />
+                </svg>
+              </button>
+              <button className="btn-icon chat__conversation-board__message__option-button option-item more-button">
+                <svg
+                  className="feather feather-more-horizontal sc-dnqmqq jxshSx"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  aria-hidden="true"
+                >
+                  <circle cx={12} cy={12} r={1} />
+                  <circle cx={19} cy={12} r={1} />
+                  <circle cx={5} cy={12} r={1} />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div className="chat__conversation-board__message-container">
+            <div className="chat__conversation-board__message__person">
+              <div className="chat__conversation-board__message__person__avatar">
+                <img
+                  src="https://randomuser.me/api/portraits/women/44.jpg"
+                  alt="Monika Figi"
+                />
+              </div>
+              <span className="chat__conversation-board__message__person__nickname">
+                Monika Figi
+              </span>
+            </div>
+            <div className="chat__conversation-board__message__context">
+              <div className="chat__conversation-board__message__bubble d-flex flex-column">
+                {" "}
+                <span className="user_name">Monika Figi (Ward Name)</span>
+
+                <span>WE MUST FIND HIM!!</span>
+              </div>
               <div className="chat__conversation-board__message__bubble">
                 {" "}
-                <span>Winamp's still an essential.</span>
+                <span>Wait ...</span>
               </div>
             </div>
             <div className="chat__conversation-board__message__options">
